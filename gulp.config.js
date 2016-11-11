7cad781124d8f41e9690516672b33d31aeb89f96
MattHoneycutt <mbhoneycutt@gmail.com>
2016-11-11 11:16:22 -0600

    Initial commit.

---
+++ gulp.config.js
@@ -0,0 +1,94 @@
+'use strict';
+var module = module || {};
+module.exports = function() {
+    const destinationBaseDir = 'www';
+    const modulesRoot = 'node_modules';
+    const appRoot = 'app';
+
+    const cssDestination = `${destinationBaseDir}/css`;
+    const jsDestination = `${destinationBaseDir}/js`;
+
+    return {
+        appRoot,
+        destinationBaseDir,
+        cssDestination,
+        jsDestination,
+        vendorScss: {
+            autoprefix: false,
+            dest: `${destinationBaseDir}/css/vendor.min.css`,
+            srcGlob: [
+                //Samples:
+                `vendor/main.scss`,
+                // `${modulesRoot}/some/other/package.scss`
+            ]
+        },
+        appScss: {
+            autoprefix: true,
+            dest: `${destinationBaseDir}/css/app.min.css`,
+            srcGlob: [
+                //Samples:
+                `${appRoot}/layout.scss`,
+                `${appRoot}/**/*.scss`
+            ]
+        },
+        vendorJs: {
+            concatOnly: true,
+            dest: `${jsDestination}/vendor.min.js`,
+            srcGlob: [
+                //Samples:
+                `${modulesRoot}/angular/angular.min.js`
+            ]
+        },
+        appJs: {
+            concatOnly: false,
+            dest: `${jsDestination}/app.min.js`,
+            srcGlob: [
+                //Samples:
+                `${appRoot}/app.module.js`,
+                `${appRoot}/**/*.module.js`,
+                `${appRoot}/**/*.js`
+            ]
+        },
+        appHtml: {
+            moduleName: 'templates',
+            templateRoot: `/${appRoot}/`,
+            dest: `${jsDestination}/app-templates.min.js`,
+            srcGlob:[
+                //Samples:
+                `${appRoot}/**/*.template.html`                
+            ]
+        },
+
+        staticAssets: {
+            base: appRoot,
+            dest: destinationBaseDir + '',
+            srcGlob:[
+                //Samples:
+                `${appRoot}/index.html`,
+                `${appRoot}/fonts/**/*`,
+                `${appRoot}/images/**/*`
+            ]
+        },
+
+        browserSync: {
+            port: 4004,
+            //NOTE: Use the 'appRoot' setting if you want BrowserSync to servce your app.
+            //      Use 'proxy' if your app is hosted by ASP.NET or something similar.
+            appRoot: 'www'
+            //proxy: 'http://localhost:61653'        
+        },
+
+        revJs: {
+            srcGlob: [
+                `${jsDestination}/*.min.js`
+            ],
+            dest: `${jsDestination}`
+        },
+        revCss: {
+            srcGlob: [
+                `${cssDestination}/*.min.css`
+            ],
+            dest: `${cssDestination}`
+        }
+    };
+};
---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+'use strict';
+const gulp = require('gulp');
+const del = require('del');
+const gulpHero = require('./node_modules/heroic-gulp/heroic.gulp.js')();
+const config = require('./gulp.config.js')();
+
+function clean() {
+    return del(config.destinationBaseDir + '**/*');
+}
+exports.clean = clean;
+
+function cssVendor() {
+    return gulpHero.runScssFor(config.vendorScss);
+}
+exports.cssVendor = cssVendor;
+
+function cssApp() {
+    return gulpHero.runScssFor(config.appScss);
+}
+exports.cssApp = cssApp;
+
+exports.css = gulp.parallel(cssVendor, cssApp);
+
+function jsVendor() {
+    return gulpHero.runJsFor(config.vendorJs);
+}
+exports.jsVendor = jsVendor;
+
+function jsApp() {
+    return gulpHero.runJsFor(config.appJs);
+}
+exports.jsApp = jsApp;
+
+exports.js = gulp.parallel(jsVendor, jsApp);
+
+function htmlTemplates() {
+    return gulpHero.runAngularTemplateCacheFor(config.appHtml);
+}
+exports.htmlTemplates = htmlTemplates;
+
+function staticAssets() {
+    return gulpHero.copyStaticAssets(config.staticAssets);
+}
+exports.staticAssets = staticAssets;
+
+function watch() {
+    //TODO: I'd like to encapsulate this, but I'm not sure what the best way to do that would be. 
+    gulp.watch(config.appScss.srcGlob, gulp.series(cssApp)).on('error', gulpHero.watchFailed('cssApp'));
+    gulp.watch(config.appJs.srcGlob, gulp.series(jsApp)).on('error', gulpHero.watchFailed('jsApp'));
+    gulp.watch(config.appHtml.srcGlob, gulp.series(htmlTemplates)).on('error', gulpHero.watchFailed('htmlTemplates'));
+    gulp.watch(config.staticAssets.srcGlob, gulp.series(staticAssets)).on('error', gulpHero.watchFailed('staticAssets'));
+}
+exports.watch = watch;
+
+function revJs() {
+    return gulpHero.runRev(config.revJs);
+}
+exports.revJs = revJs;
+
+function revCss() {
+    return gulpHero.runRev(config.revCss);
+}
+exports.revCss = revCss;
+
+exports.revFiles = gulp.parallel(revJs, revCss);
+
+function revReplace () {
+    var target = config.destinationBaseDir + '/index.html';
+    return gulpHero.runRevReplace(config.revCss, config.revJs, target);
+}
+exports.revReplace = revReplace;
+
+exports.cacheBust = gulp.series(exports.revFiles, revReplace);
+
+function browserSync () {
+    gulpHero.runBrowserSync(config.browserSync, config.appScss.dest, config.appJs.dest, config.appHtml.dest, config.staticAssets.srcGlob);
+}
+exports.browserSync = browserSync;
+
+exports.build = gulp.series(clean, gulp.parallel(exports.js, exports.css, htmlTemplates, staticAssets));
+
+exports.buildAndServe = gulp.series(exports.build, gulp.parallel(watch, browserSync));
